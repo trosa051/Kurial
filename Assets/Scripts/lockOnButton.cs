519bb3862f37ae7b33fef290d78e6bfc85d28933
trosa051 <trosa051@live.kutztown.edu>
2020-11-29 23:41:14 -0500

    prototype scene movement
    Partial code from https://assetstore.unity.com/packages/templates/systems/easy-character-movement-57985

---
+++ Assets/Scripts/lockOnButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockOnButton : MonoBehaviour
+{
+
+        [Tooltip("The keyboard key to lock fields.")]
+        [SerializeField]
+        private KeyCode _lockKey = KeyCode.Escape;
+
+        [Tooltip("Does the variable start out locked?")]
+        [SerializeField]
+        private bool _locked;
+
+        [Tooltip("Floats to lock")]
+        [SerializeField]
+        private Transform[] toLock;
+
+        [Tooltip("Floats to lock")]
+        [SerializeField]
+        private Transform[] currentNums;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(_lockKey))
+            _locked = !_locked;
+
+        if (!_locked) {
+            for(int i = 0; i < toLock.Length; i++){
+                currentNums[i] = toLock[i];
+            }
+        }
+        if (_locked){
+            for(int i = 0; i < toLock.Length; i++){
+                toLock[i] = currentNums[i];
+            }
+        }
+    }
+}
