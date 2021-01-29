3867a578f7689047958b85f3afb61d0adf23423a
trosa051 <trosa051@live.kutztown.edu>
2021-01-29 11:58:47 -0500

    rot

---
+++ Assets/Scripts/rot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rot : MonoBehaviour
+{
+    [SerializeField]
+    float xAngle = 1f;
+    [SerializeField]
+    float yAngle = 1f;
+    [SerializeField]
+    float zAngle = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
+    }
+}
