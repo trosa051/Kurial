665a0522aac026e252273863a801dcc24aa600db
trosa051 <trosa051@live.kutztown.edu>
2021-02-18 11:38:42 -0500

    Selection 78% done
    Save Often, Save Lives

---
+++ Assets/Scripts/RoomSelection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomSelection : MonoBehaviour
+{
+    [SerializeField]
+    Dropdown roomType = null;
+    [SerializeField]
+    GameObject[] Sels = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomType.value = 0;
+        Changed();
+    }
+
+    // Update is called once per frame
+    public void Changed()
+    {
+    Debug.Log(roomType.captionText.text+" has been selected.");
+    
+    foreach (GameObject i in Sels){
+        i.SetActive(false);
+    }
+
+    switch (roomType.value)
+      {
+        case 0:
+            Sels[0].SetActive(true);
+            break;
+        case 1:
+            Sels[1].SetActive(true);
+            break;
+        case 2:
+            Sels[2].SetActive(true);
+            break;
+        case 3:
+            Sels[3].SetActive(true);
+            break;
+        case 4:
+            Sels[4].SetActive(true);
+            break;
+          default:
+            Debug.Log("Default case");
+            break;
+      }
+    }
+}
+ 
+
+ /*
+ Notes for self:
+
+ This is where I can append the correct information to transfer the right prefab info into the scene
+ */
