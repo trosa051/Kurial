914002633c9a03f2827e6d780eb4cb74840a69f8
gzmn <71097621+gzmn@users.noreply.github.com>
2021-03-17 22:31:48 -0400

    login system progress

---
+++ Assets/Scripts/KuratorSys.cs
@@ -0,0 +1,55 @@
+﻿//Kurator Register and Login
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KuratorSys : MonoBehaviour
+{
+	[SerializeField] string URL;
+	//WWWForm login;
+	[SerializeField] InputField Username;
+	[SerializeField] InputField Password;
+	[SerializeField] Button LoginButton;
+	
+	[SerializeField] Text ErrorMessage;
+	
+	WWWForm form;
+
+	public void KuratorLogin(){
+		LoginButton.interactable = false;
+		StartCoroutine (Login());
+		
+	}
+	
+	IEnumerator Login(){
+		form = new WWWForm ();
+		form.AddField("username", Username.text);
+		form.AddField("password", Password.text);
+		
+		WWW Kurial = new WWW (URL, form);
+		yield return Kurial;
+		
+		if (Kurial.error != null) {
+			ErrorMessage.text = "error";
+			Debug.Log("<color=red>"+Kurial.text+"</color>");
+		} else {
+			if (Kurial.isDone) {
+				if (Kurial.text.Contains ("error")) {
+					ErrorMessage.text = "invalid username or password!";
+					Debug.Log("<color=red>"+Kurial.text+"</color>");//error
+				} else {
+					SceneManager.LoadScene(0);
+					Debug.Log("<color=green>"+Kurial.text+"</color>");//user exist
+				}
+			}
+		}
+		
+		LoginButton.interactable = true;
+		Kurial.Dispose();
+		
+	}
+}
