42ffe1472d1816b218c09a7982496cc086fbd4d9
trosa051 <trosa051@live.kutztown.edu>
2021-04-21 00:35:33 -0400

    Added Kuration Link Display

---
+++ Assets/copyText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copyText : MonoBehaviour
+{
+    sel go;
+
+    public void onClick()
+    {
+        go = GameObject.Find("_manager").GetComponent<sel>();
+        GUIUtility.systemCopyBuffer = "https://kurial.space/?kuration="+go.kurID;
+        //WebGLCopyAndPasteAPI.PassCopyToBrowser("https://kurial.space/?kuration="+go.kurID);
+        Debug.Log("https://kurial.space/?kuration="+go.kurID+" Copied to clipboard");
+    }
+}
---
+++ Assets/linkGen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class linkGen : MonoBehaviour
+{
+    public InputField inFi;
+    sel go;
+
+    void Start()
+    {
+        go = GameObject.Find("_manager").GetComponent<sel>();
+        inFi.text = inFi.text+go.kurID;
+    }
+
+}
